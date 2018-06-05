d427f5b4e505d30bd787efac72b71c26e6eca46e
ralph.dworzanski@gmail.com <Ganymed5>
2018-06-05 11:52:07 +0200

    Added GLM as dependency

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "pugixml.hpp"
+#include "../lib/glm/glm.hpp"
 
 #include "Background.h"
 #include "Camera.h"
@@ -14,24 +15,25 @@ int main(int argc, char** argv) {
     }
 
     pugi::xml_node scene = doc.child("scene");
-    std::cout << scene.name() << "\n\n";
+    std::cout << scene.name() << "\n";
 
     for(pugi::xml_node item = scene.first_child(); item; item = item.next_sibling()) {
         std::cout << item.name() << "\n";
 
-        for(pugi::xml_attribute attr = item.first_attribute(); attr; attr = attr.next_attribute()) {
-            std::cout << " " << attr.name() << "=" << attr.value() << "\n";
-
-            std::string attrName = attr.name();
-        }
-
         for(pugi::xml_node child = item.first_child(); child; child = child.next_sibling()) {
             for(pugi::xml_attribute attr = child.first_attribute(); attr; attr = attr.next_attribute()) {
-                std::cout << " " << child.name();
+                std::cout << "   " << child.name();
                 std::cout << " " << attr.name() << "=" << attr.value() << "\n";
                 std::string attrName = attr.name();
             }
         }
+
+        for(pugi::xml_attribute attr = item.first_attribute(); attr; attr = attr.next_attribute()) {
+            std::cout << "  " << attr.name() << "=" << attr.value() << "\n";
+
+            std::string attrName = attr.name();
+        }
+
     }
     return 0;
 }
